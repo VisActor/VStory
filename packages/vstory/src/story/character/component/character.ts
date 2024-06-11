443f808f63d94116bf82e511c936e12d30c5de2d
zhouxinyu <zhouxinyu12138@gmail.com>
2024-06-11 15:48:24 +0800

    feat: support zindex

--- packages/vstory/src/story/character/component/character.ts
+++ packages/vstory/src/story/character/component/character.ts
@@ -34,7 +34,11 @@ export abstract class CharacterComponent extends CharacterBase {
   }
 
   protected _initGraphics(): void {
-    this._group = createGroup({ ...getLayoutFromWidget(this._spec.position), angle: this._spec.options.angle });
+    this._group = createGroup({
+      ...getLayoutFromWidget(this._spec.position),
+      angle: this._spec.options.angle,
+      zIndex: this._spec.zIndex
+    });
     this.option.graphicParent.add(this._group);
 
     this._graphic = this._createGraphic();
@@ -91,60 +95,64 @@ export abstract class CharacterComponent extends CharacterBase {
   }
 }
 
-export abstract class CharacterGraphicComponent extends CharacterBase {
-  protected declare _spec: IComponentCharacterSpec;
+// export abstract class CharacterGraphicComponent extends CharacterBase {
+//   protected declare _spec: IComponentCharacterSpec;
 
-  get spec() {
-    return this._spec;
-  }
-  protected declare _graphic: Graphic;
-  get graphic() {
-    return this._graphic;
-  }
+//   get spec() {
+//     return this._spec;
+//   }
+//   protected declare _graphic: Graphic;
+//   get graphic() {
+//     return this._graphic;
+//   }
 
-  protected declare _group: IGroup;
-  get group() {
-    return this._group;
-  }
+//   protected declare _group: IGroup;
+//   get group() {
+//     return this._group;
+//   }
 
-  protected abstract _createGraphic(): Graphic;
+//   protected abstract _createGraphic(): Graphic;
 
-  protected _initRuntime(): void {
-    return;
-  }
+//   protected _initRuntime(): void {
+//     return;
+//   }
 
-  protected _parserSpec(): void {
-    return;
-  }
+//   protected _parserSpec(): void {
+//     return;
+//   }
 
-  protected _initGraphics(): void {
-    this._group = createGroup({ ...getLayoutFromWidget(this._spec.position), angle: this._spec.options.angle });
-    this.option.graphicParent.add(this._group);
+//   protected _initGraphics(): void {
+//     this._group = createGroup({
+//       ...getLayoutFromWidget(this._spec.position),
+//       angle: this._spec.options.angle,
+//       zIndex: this._spec.zIndex
+//     });
+//     this.option.graphicParent.add(this._group);
 
-    this._graphic = this._createGraphic();
-    this._graphic.init();
+//     this._graphic = this._createGraphic();
+//     this._graphic.init();
 
-    this._graphic.applyGraphicAttribute(this._spec.options.graphic);
+//     this._graphic.applyGraphicAttribute(this._spec.options.graphic);
 
-    this._graphic.applyLayoutData(this._spec.position);
-    this.hide();
-  }
+//     this._graphic.applyLayoutData(this._spec.position);
+//     this.hide();
+//   }
 
-  show(): void {
-    this._graphic?.show();
-  }
-  hide(): void {
-    this._graphic?.hide();
-  }
+//   show(): void {
+//     this._graphic?.show();
+//   }
+//   hide(): void {
+//     this._graphic?.hide();
+//   }
 
-  getGraphicParent() {
-    return this._group;
-  }
+//   getGraphicParent() {
+//     return this._group;
+//   }
 
-  clearCharacter(): void {
-    if (this._group) {
-      this._group.parent.removeChild(this._group);
-      this._graphic = null;
-    }
-  }
-}
+//   clearCharacter(): void {
+//     if (this._group) {
+//       this._group.parent.removeChild(this._group);
+//       this._graphic = null;
+//     }
+//   }
+// }
