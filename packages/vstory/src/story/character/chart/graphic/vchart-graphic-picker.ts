3f415953cf92cec0111116d9239e7814024475e6
zhouxinyu <zhouxinyu12138@gmail.com>
2024-06-13 14:57:46 +0800

    fix: fix issue with pick

--- packages/vstory/src/story/character/chart/graphic/vchart-graphic-picker.ts
+++ packages/vstory/src/story/character/chart/graphic/vchart-graphic-picker.ts
@@ -17,18 +17,20 @@ import type {
   IGraphicRender,
   IPickParams
 } from '@visactor/vrender-core';
-import { CHART_NUMBER_TYPE, Chart } from './vchart-graphic';
+import type { Chart } from './vchart-graphic';
+import { CHART_NUMBER_TYPE } from './vchart-graphic';
 
 @injectable()
 export class VChartPicker implements IGraphicPicker {
   type = 'chart';
   numberType: number = CHART_NUMBER_TYPE;
 
-  constructor() {}
-
   contains(chart: any, point: any, params?: IPickParams): boolean | any {
     const vChart = (chart as Chart).vchart;
     const chartStage = vChart.getStage();
+    // point转成全局坐标
+    const globalPoint = { x: 0, y: 0 };
+    chart.parent.globalTransMatrix().getInverse().transformPoint(point, globalPoint);
     // @ts-ignore
     chartStage._editor_needRender = true;
     const matrix = chart.globalTransMatrix.clone();
@@ -37,7 +39,7 @@ export class VChartPicker implements IGraphicPicker {
     chartStage.window.setViewBoxTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f);
     chartStage.dirtyBounds?.clear();
     const nextP = { x: 0, y: 0 };
-    matrix.transformPoint(point, nextP);
+    matrix.transformPoint(globalPoint, nextP);
     const graphic = chartStage.pick(nextP.x, nextP.y);
 
     return graphic;
