598f11fec897a425091d030263713f1e6e84280e
Kuiliang Zhang <z1219202167@outlook.com>
2024-07-18 23:42:59 -0700

    chore: reverse import name in vite.config.ts

--- packages/vstory/demo/vite.config.ts
+++ packages/vstory/demo/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
-import localConf from './vite.config.local.ts';
+import localConf from './vite.config.local';
 
 // https://vitejs.dev/config/
 export default defineConfig({
