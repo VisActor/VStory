62b666ad4c78e35a8e3f2667a2e90d7b7d1bf159
zhouxinyu <zhouxinyu12138@gmail.com>
2024-10-10 16:30:28 +0800

    feat: export richtext msg

--- packages/vstory/src/edit/edit-component/edit-control/richtext-control.ts
+++ packages/vstory/src/edit/edit-component/edit-control/richtext-control.ts
@@ -22,11 +22,16 @@ export class RichTextControl {
   }
 
   onRichTextPluginEdit = (msg: { type: string; p: RichTextEditPlugin }) => {
-    this._edit.emitter.emit(EditActionEnum.richTextPluginEdit, msg);
+    this._edit.emitter.emit(EditActionEnum.richTextPluginEdit, { data: msg, character: this._character });
     const { type, p } = msg;
     if (p.currRt !== this._richText) {
       console.warn('current edit richtext not match in richtext-control.onRichTextPluginEdit');
       return;
+    }
+    if (msg.type === 'change' && this._character) {
+      this._character.setConfig({
+        options: { graphic: { textConfig: this._richText.attribute.textConfig } }
+      });
     }
     // do noting 富文本编辑消息的处理
     // console.log('onRichTextPluginEdit', type, p);
