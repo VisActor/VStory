941ec9d60c4b98f8711e458b52103e770ce4a6b1
Rui-Sun <sunrui1992@foxmail.com>
2024-12-17 15:34:39 +0800

    docs: add source-of-new-contacts-area-chart demo

--- packages/vstory/demo/src/App.tsx
+++ packages/vstory/demo/src/App.tsx
@@ -42,6 +42,7 @@ import { NationalMemorial } from './demos/works/NationalMemorial';
 import { BarWorkingInSameIndustry } from './demos/infographic/bar-work-in-same-industry';
 import { ProjectGoal } from './demos/infographic/project-goal';
 import { BigDataWordCloud } from './demos/infographic/big-data-wordcloud';
+import { AreaChart } from './demos/infographic/source-of-new-contacts-area-chart';
 
 type MenusType = (
   | {
@@ -249,6 +250,10 @@ const App = () => {
         {
           name: 'BigDataWordCloud',
           component: BigDataWordCloud
+        },
+        {
+          name: 'Source of New Contacts Area Chart',
+          component: AreaChart
         }
       ]
     },
