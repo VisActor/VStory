61bbac2f53272850244f1c49ab758fa6bdca1673
xiaoyvhuv <xiaoyvhuv@sina.com>
2025-03-03 11:46:12 +0800

    docs: add infographic demo: film_analyze

--- common/autoinstallers/lint/commit-lint.js
+++ common/autoinstallers/lint/commit-lint.js
@@ -19,6 +19,17 @@ const result = child_process.spawnSync(
       ? `${commitlintBinPath} --config "${configPath}" --cwd "${path.dirname(gitPath)}" --edit`
       : `"${commitlintBinPath}" --config "${configPath}" --cwd "${path.dirname(gitPath)}" --edit`
   ],
+  {
+    stdio: 'inherit',
+    shell: true
+  }
+  process.platform === 'win32' ? 'cmd' : 'sh',
+  [
+    process.platform === 'win32' ? '/c' : '-c',
+    process.platform === 'win32'
+      ? `${commitlintBinPath} --config "${configPath}" --cwd "${path.dirname(gitPath)}" --edit`
+      : `"${commitlintBinPath}" --config "${configPath}" --cwd "${path.dirname(gitPath)}" --edit`
+  ],
   process.platform === 'win32'
     ? {
         stdio: 'inherit',
