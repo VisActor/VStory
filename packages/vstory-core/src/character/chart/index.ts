575935fb38daf07cc318165cf4f3371741c12e9c
lixuefei.1313 <lixuef1313@163.com>
2025-09-11 14:26:05 +0800

    feat: change ranking bar to vstory-external

--- packages/vstory-core/src/character/chart/index.ts
+++ packages/vstory-core/src/character/chart/index.ts
@@ -1,6 +1,5 @@
 export * from './character-chart';
 export * from './graphic/vchart-graphic';
 export * from './character/vchart';
-export * from './character/rankingBar';
 export * from './character/scatter-bar';
 export * from './character/wave-scatter';
